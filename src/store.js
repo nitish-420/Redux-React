5670b538706dded674d1e54e499c23c1a50f9ded
Nitish Bhardwaj <67393030+nitish-420@users.noreply.github.com>
2022-01-04 04:07:12 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,17 @@
 import {createStore} from "redux";
 import rootReducer from "./reducers";
 
+// import { composeWithDevTools } from 'redux-devtools-extension'
 
+// import thunk from "redux-thunk" 
+
+// const composedEnhancer = composeWithDevTools(applyMiddleware(thunk))
+
+
+// const store = createStore(
+//     rootReducer,
+//     composedEnhancer
+// );
+// I know why I added this in comments here ;)
 const store=createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store
